dd0690c4947ed9f665e65c6e358221191673e392
ckinasch <ckinasch@proton.me>
2023-01-09 13:32:14 +1100

    Slice of random ints function; beginning PopulateBoard

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 var _ = errors.New
@@ -52,8 +53,38 @@ func PrintBoard(b *Board) {
 	}
 }
 
+// Return a slice of randomly generated distinct integers; all values are incremented
+func GetRandomVals() []int {
+
+	vals := rand.Perm(9)
+	for i := range vals {
+		vals[i]++
+	}
+	return vals
+
+}
+
+func PopulateBoard(b *Board) {
+
+	var _ = GetRandomVals()
+
+	for r, row := range b.cells {
+		for c := range row {
+			cur := &b.cells[r][c]
+			cur.value = r
+		}
+	}
+
+}
+
 func main() {
+
+	PopulateBoard(&b)
 
 	PrintBoard(&b)
 
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
 }
