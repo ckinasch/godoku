bd4c06442ad419ff540f44aa1a67848c83a24e4e
ckinasch <ckinasch@proton.me>
2023-01-09 13:05:40 +1100

    defined base types for board

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 )
 
-func main(){
-	fmt.Println("Hello godoku")
+var _ = errors.New
+var _ = fmt.Println
+var _ = rand.Perm
+
+
+type Cell struct {
+	row   int
+	col   int
+	value int
+	box   Box
+}
+
+type Box struct {
+	index int
+	vals  [9]int
+}
+
+type Board struct {
+	cells [9][9]Cell
+	boxes [9]Box
+}
+
+var b Board
+
+func main() {
+
 }
