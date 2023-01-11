bf25e9136994cde7e27cad28bbbf5e92434c3286
ckinasch <99946707+ckinasch@users.noreply.github.com>
2023-01-12 09:42:25 +1100

    dev (#1)
    * Started populatedboxes
    
    * simplified box population
    
    * simplified box population ordering
    
    * conflict detection for rows and columns
    
    * creating conflict cases
    
    * Refactoring:
    converted functions to struct methods;
    simplified conflict checking
    
    * Improved indexing;
    - Boxes now indexed with cell references
    - board now contains references to cells and boxes
    
    * correctly assigned pointers to Cells in structs;
    new PopulateBox method begin
    
    * Board generation successful;
    Implemented recursive back tracking function;
    cleaned up and commented
    
    * Cleaning; Refactoring

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	crypto_rand "crypto/rand"
 	"encoding/binary"
-	_ "errors"
 	"fmt"
 	math_rand "math/rand"
-	_ "time"
+	"time"
 )
 
 type Cell struct {
@@ -18,24 +17,23 @@ type Cell struct {
 
 type Box struct {
 	index int
-	vals  [9]int
+	cells [9]*Cell
 }
 
 type Board struct {
-	cells [9][9]Cell
+	cells [9][9]*Cell
 	boxes [9]Box
 }
 
-var b Board
+var board Board
 
-func PrintBoard(b *Board) {
+func (b *Board) PrintBoard() {
 	lines := "---------------------"
 	fmt.Println(lines)
 	for r, row := range b.cells {
 		for c := range row {
 			cur := b.cells[r][c]
 			fmt.Print("|")
-			// fmt.Printf("%d:%d", cur.value, cur.box)
 			fmt.Printf("%d", cur.value)
 
 			if (c+1)%3 == 0 {
@@ -54,25 +52,29 @@ func PrintBoard(b *Board) {
 
 // Simultaneous calls for new random slices may overlap; time based seed is insufficient;
 // Cryptographically secure approach to ensure unique slices
-func seedRand() {
+func crypto_seed() int64 {
 	// https://stackoverflow.com/a/54491783
 
 	var b [8]byte
-	_, err := crypto_rand.Read(b[:])
 
-	if err != nil {
+	if _, err := crypto_rand.Read(b[:]); err != nil {
 		panic("Cannot seed")
 	}
+	return int64(binary.LittleEndian.Uint64(b[:]))
+}
 
-	seedval := int64(binary.LittleEndian.Uint64(b[:]))
+func time_seed() int64 {
+	return time.Now().Unix()
+}
 
-	math_rand.Seed(seedval)
+func seedRand() {
+
+	// math_rand.Seed(crypto_seed())
+	math_rand.Seed(time_seed())
 }
 
 // Return a slice of randomly generated distinct integers; all values are incremented
 func GetRandomVals() []int {
-
-	seedRand()
 
 	vals := math_rand.Perm(9)
 	for i := range vals {
@@ -82,58 +84,131 @@ func GetRandomVals() []int {
 
 }
 
-// Populate boxes 0, 4, 8; they are diagonal and therefore independant.
-// This completes as much of the board as possible before attempting to brute force the remaining boxes;
-// (theoretically) increasing the chance of success
-func SeedInitialBoxes(b *Board) {
+// Replace the first element of a slice with the last one and return a slice with the tail cut off
+func removeSliceZero(s []int) []int {
+	s[0] = s[len(s)-1]
+	return s[:len(s)-1]
+}
 
-	// 2d array of random integers used to seed boxes 0, 4, 8 (diagonal)
-	vals := [3][]int{GetRandomVals(), GetRandomVals(), GetRandomVals()}
+func (c *Cell) CheckValueConflict(val int) bool {
 
-	// Complementary Array of indices for incrementing the vals array when val has been used
-	i := [3]int{0, 0, 0}
-
-	for r, row := range b.cells {
-		for c := range row {
-			cur := &b.cells[r][c]
-			if cur.box == 0 {
-				cur.value = vals[0][i[0]]
-				i[0]++
-			}
-			if cur.box == 4 {
-				cur.value = vals[1][i[1]]
-				i[1]++
-			}
-			if cur.box == 8 {
-				cur.value = vals[2][i[2]]
-				i[2]++
-			}
+	for i := 0; i < 9; i++ {
+		if val == board.cells[i][c.col].value {
+			return true
 		}
+		if val == board.cells[c.row][i].value {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Box) ContainsValue(val int) bool {
+
+	for i := 0; i < len(b.cells); i++ {
+		if b.cells[i].value == val {
+			return true
+		}
+	}
+	return false
+
+}
+
+// Populate the structs with references to cells;
+// Index by Box and Board position to simplify processing and access
+func (b *Board) IndexBoard() {
+
+	for i := 0; i < 81; i++ {
+		row := i / 9
+		col := i % 9
+		// Index the box
+		b.boxes[row].index = row
+
+		// Get the box position
+		ibox := row/3*3 + col/3
+
+		// Store reference to current created cell at this position
+		cell := &Cell{row: row, col: col, box: ibox}
+
+		// Create reference to cell in board row/col
+		b.cells[row][col] = cell
+
+		// Create reference to cell in appropriate box and position in the boxes index
+		b.boxes[ibox].cells[row%3*3+col%3] = cell
+
 	}
 
 }
 
-func PopulateBoard(b *Board) {
+// Check if the board has been completed
+func (b *Board) CheckBoard() bool {
 
-	for r, row := range b.cells {
-		for c := range row {
-			box := r/3*3 + c/3
-			cur := &b.cells[r][c]
-			cur.box = box
+	for i := 0; i < 81; i++ {
+		row := i / 9
+		col := i % 9
+		if b.cells[row][col].value == 0 {
+			// Board incomplete
+			return false
+		}
+	}
+	// Board has been completed, trigger recursive exit
+	return true
+}
+
+// Recursive backtracking function
+// Begins placing a random sequence and placing valid values
+// Backtraces as far as required to create a valid solution
+func (b *Board) FillBoard() bool {
+
+	for i := 0; i < 81; i++ {
+		row := i / 9
+		col := i % 9
+		// Refer to current cell
+		cell := b.cells[row][col]
+
+		if cell.value == 0 {
+
+			// Next set of values to attempt placement
+			// Iterate and recursively place
+			vals := GetRandomVals()
+			for _, v := range vals {
+
+				// Value is valid and can be placed
+				if !cell.CheckValueConflict(v) && !b.boxes[cell.box].ContainsValue(v) {
+					cell.value = v
+
+					// Board is completed
+					if b.CheckBoard() {
+						return true
+
+					} else {
+						if b.FillBoard() {
+							return true
+						}
+					}
+				}
+			}
+			// No solution from here; reset the current cell's value and break loop
+			b.cells[row][col].value = 0
+			break
 		}
 	}
 
-	SeedInitialBoxes(b)
+	// Trace back to previous chain and attempt a different value
+	return false
 
 }
 
 func main() {
 
-	PopulateBoard(&b)
+	board.IndexBoard()
 
-	PrintBoard(&b)
+	board.FillBoard()
+	board.PrintBoard()
 
 }
 
 func init() {
+	seedRand()
+
 }
